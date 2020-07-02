335749f1459cac5e3afc4277f7a83e7725b53fe9
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-07-02 17:40:39 -0300

    App - Telas

--- Projeto/Telas - App/Campeonato_App/Campeonato_App/Campeonato_App/App.xaml.cs
+++ Projeto/Telas - App/Campeonato_App/Campeonato_App/Campeonato_App/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Campeonato_App
         {
             InitializeComponent();
 
-            MainPage = new CadastroView();
+            MainPage = new LoginView();
         }
 
         protected override void OnStart()
